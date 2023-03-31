31acf8066ac2113f5659e321e82ea7ba2be5853f
Miro Tomasik <mirotomasik@gmail.com>
2023-03-31 15:23:39 +0200

    update

---
+++ float/main.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+from os import environ, chdir, system
+from dbt.tests.util import run_dbt
+
+
+def setup_profile():
+    client = boto3.client('secretsmanager')
+
+    secrets = client.get_secret_value(
+        SecretId="iap/dev"
+    )['SecretString']
+
+    for key, val in json.loads(secrets).items():
+        environ[key] = val
+        print(key, val)
+
+
+
+def handler(event, context):
+    setup_profile()
+
+    chdir("jaffle_shop")
+
+    system("dbt deps")
+    system("dbt build")
+    system("dbt run")
+
+    print("All good.")
+
+handler(None, None)
